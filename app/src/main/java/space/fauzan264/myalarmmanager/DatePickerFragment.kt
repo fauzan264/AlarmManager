ba428bdf7de70824b44342307b57b6f244e26939
fauzan264 <fauzan.c4@gmail.com>
2020-11-04 01:05:56 +0700

    Initial commit

---
+++ app/src/main/java/space/fauzan264/myalarmmanager/DatePickerFragment.kt
@@ -0,0 +1,53 @@
+package space.fauzan264.myalarmmanager
+
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.DatePicker
+import androidx.fragment.app.DialogFragment
+import java.time.Month
+import java.util.*
+
+class DatePickerFragment : DialogFragment(), DatePickerDialog.OnDateSetListener {
+
+    private var mListener: DialogDateListener? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        if (context != null) {
+            mListener = context as DialogDateListener?
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+
+        if (mListener != null) {
+            mListener = null
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val date = calendar.get(Calendar.DATE)
+
+        return DatePickerDialog(activity as Context, this, year, month, date)
+    }
+
+    interface DialogDateListener {
+        fun onDialogDateSet(tag: String?, year: Int, month: Int, dayOfMonth: Int)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+        mListener?.onDialogDateSet(tag, year, month, dayOfMonth)
+    }
+
+}
